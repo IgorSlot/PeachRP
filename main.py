28c2a8fdfa3479f8d96c086a6b0f5aae26d58a84
IgorSlot <111352987+IgorSlot@users.noreply.github.com>
2023-03-10 14:32:26 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ from pypresence import Presence
 import configparser
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read('config.ini') 
 
 RPC = Presence(f"{config['Identifiers']['ClientID']}")
  
 RPC.connect()
 RPC.update(
-    state=f"{config['State']['State']}",
-    details=f"{config['State']['Details']}",
-    start=f"{config['State']['StartTimestamp']}",
-    end=f"{config['State']['EndTimestamp']}",
-    large_image=f"{config['Images']['LargeImage']}",
-    large_text=f"{config['Images']['LargeImageTooltip']}",
-    small_image=f"{config['Images']['SmallImage']}",
-    small_text=f"{config['Images']['SmallImageTooltip']}"
+    state=None if f"{config['State']['State']}" == "" else f"{config['State']['State']}",
+    details=None if f"{config['State']['Details']}" == "" else f"{config['State']['Details']}",
+    start=None if f"{config['State']['StartTimestamp']}" == "" else f"{config['State']['StartTimestamp']}",
+    end=None if f"{config['State']['EndTimestamp']}" == "" else f"{config['State']['EndTimestamp']}",
+    large_image=None if f"{config['Images']['LargeImage']}" == "" else f"{config['Images']['LargeImage']}",
+    large_text=None if f"{config['Images']['LargeImageTooltip']}" == "" else f"{config['Images']['LargeImageTooltip']}",
+    small_image=None if f"{config['Images']['SmallImage']}" == "" else f"{config['Images']['SmallImage']}",
+    small_text=None if f"{config['Images']['SmallImageTooltip']}" == "" else f"{config['Images']['SmallImageTooltip']}"
 )
  
 input()
